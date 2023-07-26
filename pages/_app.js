a47f08424f2e5a764e7cfe4ac304b33520e4440d
washira5lab <washira@5lab.co>
2023-07-26 22:33:49 +0700

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,44 @@
-import '@/styles/globals.css'
+import "../styles/globals.css";
+import { useState, useEffect } from "react";
+import liff from "@line/liff";
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }) {
+  const [liffObject, setLiffObject] = useState(null);
+  const [liffError, setLiffError] = useState(null);
+
+  // Execute liff.init() when the app is initialized
+  useEffect(() => {
+    // to avoid `window is not defined` error
+    // import("@line/liff").then((liff) => {
+    //   console.log("LIFF init...");
+    //   liff
+    //     .init({ liffId: process.env.NEXT_PUBLIC_LIFF_ID })
+    //     .then(() => {
+    //       console.log("LIFF init succeeded.");
+    //       setLiffObject(liff);
+    //     })
+    //     .catch((error) => {
+    //       console.log("LIFF init failed.");
+    //       setLiffError(error.toString());
+    //     });
+    // });
+
+    liff.init({ liffId: process.env.NEXT_PUBLIC_LIFF_ID })
+      .then(() => {
+        console.log("LIFF init succeeded.");
+        setLiffObject(liff);
+      })
+      .catch((error) => {
+        console.log("LIFF init failed.");
+        setLiffError(error.toString());
+      });
+  }, []);
+
+  // Provide `liff` object and `liffError` object
+  // to page component as property
+  pageProps.liff = liffObject;
+  pageProps.liffError = liffError;
+  return <Component {...pageProps} />;
 }
+
+export default MyApp;
